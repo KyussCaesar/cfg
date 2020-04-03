a69ec249bc52c081c6b5264c10081fe1f34062d3
Antony Southworth <southworthy@gmail.com>
2020-04-04 00:07:10 +1300

    use safer pwd

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 # Setup for my rc files
 #
 # Symlinks bashrc and sets global gitignore
 # Must be run from the repo root.
-ln -s `pwd`/bashrc ~/.bashrc
-git config --global core.excludesfile `pwd`/gitignore_global
+ln -s "$(pwd)"/bashrc ~/.bashrc
+git config --global core.excludesfile "$(pwd)"/gitignore_global
 
